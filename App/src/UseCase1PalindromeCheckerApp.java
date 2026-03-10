67a20a759f16f183484c388c59e0bfc4c906f635
scsecore <scsecore@gmail.com>
2026-03-10 09:26:35 +0530

    Initial project setup

---
+++ App/src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Welcome to Palindrome Checker Management System ");
+        System.out.println("=================================");
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : v1.0");
+        System.out.println("Author : Developer");
+        System.out.println("---------------------------------");
+        System.out.println("System initialized successfully.");
+        System.out.println("---------------------------------");
+
+    }
+
+}
